d8a5ad315bf0c4856d8f4aaa069998e6ff04eb6d
Rissa <charissa.jackson928@gmail.com>
2020-08-06 01:02:31 -0500

    added gymnastic handstand picture

--- index.php
+++ index.php
@@ -12,10 +12,11 @@ require_once('Contortion.php');
 $gymnastic = new Gymnastic('straight', 'strength');
 $contortion = new Contortion('curved', 'flexibility');
 echo '<b>Gymnastic Handstand</b> <br>';
-echo 'The gymnastic handstand shape is ' . $gymnastic->get_body() . ' and the focus is on ' . $gymnastic->get_focus() . '<br><br>';
+echo "<b><img src='gymnastic.jpg' alt='gymnastic handstand' /></b> <br>"; 
+echo 'The gymnastic handstand shape is ' . $gymnastic->get_body() . ' and the focus is on ' . $gymnastic->get_focus() . '.' . '<br><br>';
 echo '<b>Contortion Handstand</b> <br>';
 echo "<b><img src='contortion.jpg' alt='contortion handstand ' /> </b> <br>"; 
-echo 'The contortion handstand shape is ' . $contortion->get_body() . ' and the focus is on ' . $contortion->get_focus();
+echo 'The contortion handstand shape is ' . $contortion->get_body() . ' and the focus is on ' . $contortion->get_focus() . '.';
 ?>
 
 </body> 
